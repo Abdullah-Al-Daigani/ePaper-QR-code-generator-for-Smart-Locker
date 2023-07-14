f4f3a9376aae99cbe431faa95d9df81ddd6da00a
Abdullah M. Al-Daigani <abdullah_null@outlook.com>
2023-07-14 04:45:25 +0300

    added name and cleaned up

--- src/main.cpp
+++ src/main.cpp
@@ -1,63 +1,41 @@
-/**
- *  QRCode
- *
- *  A quick example of generating a QR code.
- *
- *  This prints the QR code to the serial monitor as solid blocks. Each module
- *  is two characters wide, since the monospace font used in the serial monitor
- *  is approximately twice as tall as wide.
- *
- */
 #include <Arduino.h>
 #include "qrcode.h"
 
-#define LILYGO_T5_V213
-#include <boards.h>
-#include <GxEPD.h>
-#include <GxDEPG0213BN/GxDEPG0213BN.h> // 2.13" b/w  form DKE GROUP
-#include <GxIO/GxIO_SPI/GxIO_SPI.h>
-#include <GxIO/GxIO.h>
-GxIO_Class io(SPI, EPD_CS, EPD_DC, EPD_RSET);
-GxEPD_Class display(io, EPD_RSET, EPD_BUSY);
+#define LILYGO_T5_V213                 //
+#include <boards.h>                    //
+#include <GxEPD.h>                     //
+#include <GxDEPG0213BN/GxDEPG0213BN.h> // include necessary libraries for the E-paper display to function
+#include <GxIO/GxIO_SPI/GxIO_SPI.h>    //
+#include <GxIO/GxIO.h>                 //
+
+GxIO_Class io(SPI, EPD_CS, EPD_DC, EPD_RSET); //
+GxEPD_Class display(io, EPD_RSET, EPD_BUSY);  // construct the display object to control the E-paper display
+
+void displayName(void);            //
+void displayQRcode(QRCode qrcode); // declare functions to use later
 
 void setup()
 {
-  Serial.begin(115200);
+  // Serial.begin(115200);
 
-  SPI.begin(EPD_SCLK, EPD_MISO, EPD_MOSI);
-  display.init(); // enable diagnostic output on Serial
+  SPI.begin(EPD_SCLK, EPD_MISO, EPD_MOSI); // start the SPI protocol
+  display.init();                          // initiate the display
+  display.setRotation(3);                  // set the rotation to landscape
 
-  // Create the QR code
-  QRCode qrcode;
-  uint8_t qrcodeData[qrcode_getBufferSize(3)];
-  qrcode_initText(&qrcode, qrcodeData, 3, 0, "checkpoint reached!");
+  QRCode qrcode;                                                //
+  uint8_t qrcodeData[qrcode_getBufferSize(3)];                  // Create the QR code
+  qrcode_initText(&qrcode, qrcodeData, 3, 0, "RUH-10005 #PIN"); //
 
-  Serial.print("\n\n\n\n");
+  displayQRcode(qrcode); // display the QR code on the display
+  displayName();         // display the name on the display
+}
 
-  // Top quiet zone
-  Serial.print("\n\n\n\n");
-  Serial.print("{{\n");
+void loop() // nothing needed in the loop function for now
+{
+}
 
-  for (uint8_t y = 0; y < qrcode.size; y++)
-  {
-    for (uint8_t x = 0; x < qrcode.size; x++)
-    {
-      if (qrcode_getModule(&qrcode, x, y))
-      {
-        Serial.print("**");
-      }
-      else
-      {
-        Serial.print("  ");
-      }
-    }
-    Serial.print("\n");
-  }
-
-  // Bottom quiet zone
-  Serial.print("\n\n\n\n");
-
-  display.setRotation(0);
+void displayQRcode(QRCode qrcode) // display the QR code on the screen
+{
   display.fillScreen(GxEPD_WHITE);
   for (uint8_t y = 0; y < qrcode.size; y++)
   {
@@ -65,16 +43,21 @@ void setup()
     {
       if (qrcode_getModule(&qrcode, x, y))
       {
-        // display.fillRect(118 - (4 * x), 4 * y, 3, 3, GxEPD_BLACK);
-        // display.fillRect(118 - (4 * x), 120 + (4 * y), 3, 3, GxEPD_BLACK);
-        display.fillRect(4 + (4 * x), 4 + (4 * y), 3, 3, GxEPD_BLACK);
-        display.fillRect(4 + (4 * x), 130 + (4 * y), 4, 4, GxEPD_BLACK);
+        display.fillRect(4 + (4 * x), 4 + (4 * y), 4, 4, GxEPD_BLACK);
       }
     }
   }
-  display.update();
 }
 
-void loop()
+void displayName(void) // display the name and location of the locker on the screen
 {
+  display.setCursor(135, 30);
+  display.setTextSize(2);
+  display.setTextColor(GxEPD_BLACK);
+  display.print("RUH-10005");
+  display.setCursor(135, 60);
+  display.print("Al-Muraba");
+  display.setCursor(155, 80);
+  display.print("Riyadh");
+  display.update();
 }
